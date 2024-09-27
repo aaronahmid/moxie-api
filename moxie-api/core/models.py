a9accddef101fc1e30c78eec2ef6af4708db7ca2
krumitz <ahmid.balogun@shipa.ng>
2024-09-27 11:05:54 +0100

    use: sql-lite3

--- moxie-api/core/models.py
+++ moxie-api/core/models.py
@@ -5,7 +5,7 @@ import uuid
 
 
 class MedSpa(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="medspas")
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=255, blank=True)
@@ -17,21 +17,29 @@ class MedSpa(models.Model):
 
 
 class Service(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    CATEGORY_CHOICES = [
+        ("FACIAL", "Facial"),
+        ("INJECTABLES", "Injectables"),
+        ("PEELS", "Peels")
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     medspa = models.OneToOneField(
-        MedSpa, on_delete=models.CASCADE, related_name="services"
+        "Medspa", on_delete=models.CASCADE, related_name="services"
     )
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    duration = models.DurationField()
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
+    duration = models.DurationField(blank=True, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, blank=True)
+    service_type = models.CharField(max_length=75, blank=True)
 
     def __str__(self):
         return self.name
 
 
 class CustomerProfile(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name="customer_profile"
     )
@@ -48,7 +56,7 @@ class Appointment(models.Model):
         ("canceled", "Canceled"),
     ]
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     medspa = models.ForeignKey(
         MedSpa, on_delete=models.CASCADE, related_name="appointments"
     )
